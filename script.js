04b97ba67c60a6611a2bddb4241a7b419c496584
TheAwesomeTechGirl <141030476+TheAwesomeTechGirl@users.noreply.github.com>
2023-08-01 12:54:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const response = document.getElementById('image1');
+
+response.addEventListener("click", (event) =>{
+    event.target.style.background = 'red'
+    event.target.style.color ="black";
+})
+
+
+const change = document.getElementById('image2');
+
+change.addEventListener("click", (event) =>{
+    event.target.style.background = 'yellow'
+    event.target.style.color ="white";
+})
+
+const change1 = document.getElementById('image3');
+
+change1.addEventListener("click", (event) =>{
+    event.target.style.background = 'green'
+    event.target.style.color ="red";
+})
+
+const change2 = document.getElementById('image4');
+
+change2.addEventListener("click", (event) =>{
+    event.target.style.background = 'brown'
+    event.target.style.color ="white";
+})
+
+const change3 = document.getElementById('image5');
+
+change3.addEventListener("click", (event) =>{
+    event.target.style.background = 'pink'
+    event.target.style.color ="white";
+})
+
+
+const change4 = document.getElementById('image6');
+
+change4.addEventListener("click", (event) =>{
+    event.target.style.background = 'black'
+    event.target.style.color ="white";
+})
+
+
+const change5 = document.getElementById('image7');
+
+change5.addEventListener("click", (event) =>{
+    event.target.style.background = 'aqua'
+    event.target.style.color ="bisque";
+})
+
+const change6 = document.getElementById('image8');
+
+change6.addEventListener("click", (event) =>{
+    event.target.style.background = 'aqua'
+    event.target.style.color ="blue";
+})
